b41604140f9c18a7125703cb625befcdf39180bf
Sharoz Tanveer <saadtanveer7@live.co.uk>
2019-09-09 00:30:10 +0500

    simple word tokenize and print(===FilteredWord===)

--- script.py
+++ script.py
@@ -1,4 +1,10 @@
 from nltk.tokenize import sent_tokenize, word_tokenize
-EXAMPLE_TEXT = "Hello Mr. Smith, how are you doing today? The weather is great, and Python is awesome. The sky is pinkish-blue. You shouldn't eat cardboard."
-for word in word_tokenize(EXAMPLE_TEXT):
-	print(word)
+from nltk.corpus import stopwords
+TEXT = """Hello Mr. Sam, how are you doing today? The weather is great, and Money heist is awesome. You shouldn't eat pancakes. The sky is pinkish-blue."""
+TokenizedWords=word_tokenize(TEXT)
+StopWords=set(stopwords.words("english"))
+FilteredWord=[word for word in TokenizedWords if word not in StopWords]
+print("===FilteredWord===")
+print(FilteredWord)
+print("===TokenizedWords===")
+print(TokenizedWords)
